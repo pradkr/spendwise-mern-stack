630b28a54aa37bc109e3ebbfc45c509a075857e9
Pradeep Kumar <prad6g@gmail.com>
2024-03-11 23:41:45 +0530

    cors issue, removed localhost:PORT and HOST

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ if(process.env.NODE_ENV === 'development') {
 if(process.env.NODE_ENV === 'production') {
     //app.use(express.static('app/build'));
     app.use(express.static('build'));
-
-    app.get('*', (req, res) => 
+    //app.get('*', (req, res) => 
+    app.get(/^\/$/, (req, res) => 
         res.sendFile(path.resolve(__dirname, 'build', 'index.html')
         ));
 }
