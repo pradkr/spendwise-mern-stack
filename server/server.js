141d22ef124cd898c74f5d33871a32346f423d21
Pradeep Kumar <prad6g@gmail.com>
2024-03-11 10:01:42 +0530

    added HOST for react app in env file and moved build to server

--- server/server.js
+++ server/server.js
@@ -21,9 +21,9 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('app/build'));
     app.get('*', (req, res) => 
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')
+        res.sendFile(path.resolve(__dirname, 'client', 'app', 'index.html')
         ));
 }
 
