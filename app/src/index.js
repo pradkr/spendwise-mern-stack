481f4205af9c258ce34bd7a4b41a7d67d18ad419
Pradeep Kumar <prad6g@gmail.com>
2024-03-29 15:07:09 +0530

    updated readme with mongodb details

--- app/src/index.js
+++ app/src/index.js
@@ -3,23 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import './index.css';
-// import { Provider } from 'react-redux';
-// import store from './store';
-
-//import AuthProvider from './context/AuthProvider'
-// function ConnectedApp() {
-//   //const { AuthProvider } = useAuthentication();
-//   return (
-//     <AuthProvider>
-//       <App />
-//     </AuthProvider>
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <AuthContextProvider>
+    <AuthContextProvider>
     {/* <Provider store={store}> */}
       {/* <ConnectedApp /> */}
       <App />
